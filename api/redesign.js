03641869a32df4620ac0a5aacffafa440c8b3f24
ngolikov7 <ndgolikov7@gmail.com>
2025-08-16 12:18:43 +0300

    Update redesign.js

--- api/redesign.js
+++ api/redesign.js
@@ -1,7 +1,3 @@
-// api/redesign.js
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(200).json({ ok: true, msg: 'Function is deployed and reachable (use POST for real work)' });
-  }
-  res.status(200).send('OK');
+  res.status(200).json({ ok: true, msg: 'Function is reachable' });
 }
